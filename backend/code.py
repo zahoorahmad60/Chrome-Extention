b408ca18823fb304e132065c33d38b7d0e1043a7
zahoorahmad60 <zah4922@gmail.com>
2025-08-28 10:27:55 +0500

    FIASS DB

--- backend/code.py
+++ backend/code.py
@@ -1,8 +1,10 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings, ChatOllama
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import 
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
 
 embedding_model = OllamaEmbeddings(model="nomic-embed-text:latest")
 llm = ChatOllama(model="deepseek-r1:1.5b")
@@ -33,3 +35,35 @@ updated_transcript
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = splitter.create_documents([updated_transcript])
+
+#embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+vector_store = FAISS.from_documents(chunks, embedding_model)
+
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables = ['context', 'question']
+)
+
+def format_docs(retrieved_docs):
+  context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+  return context_text
+
+parallel_chain = RunnableParallel({
+    'context': retriever | RunnableLambda(format_docs),
+    'question': RunnablePassthrough()
+})
+
+parser = StrOutputParser()
+
+main_chain = parallel_chain | prompt | llm | parser
+
+main_chain.invoke('Can you summarize the video')
