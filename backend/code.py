3ee2cb32f3d99e368e7a6a50a842bea8227661e4
zahoorahmad60 <zah4922@gmail.com>
2025-08-29 17:05:32 +0500

    updating code

--- backend/code.py
+++ backend/code.py
@@ -20,13 +20,11 @@ fetched_transcript = ytt_api.fetch(video_id)
 transcript = []
 # is iterable
 for snippet in fetched_transcript:
-   # print(snippet.text)
     transcript.append(snippet.text)
 
 # indexable
 last_snippet = fetched_transcript[-1]
 
-# provides a length
 snippet_count = len(fetched_transcript)
 transcript
 
